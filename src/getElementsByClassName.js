f12fe864bdebfea20e3532c4d11b33035d3a9262
Monica Smith <monicasmith@Monicas-MacBook-Air-3.local>
2018-02-28 14:28:15 -0800

    Second commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,29 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+
+  var initialNode = document.body;
+
+  var nodeSplitter = function(node) {
+
+      var buildElementArr = [];
+
+    if (node.classList && node.classList.contains(className)) {
+        buildElementArr.push(node);
+      }
+
+    for (var i = 0; i < node.children.length; i++) {
+      var childNode = nodeSplitter(node.children[i]);
+      buildElementArr = [...buildElementArr, ...childNode];
+
+    };
+
+    return buildElementArr;
+
+  };
+
+  return nodeSplitter(initialNode);
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,69 @@
 
 // but you don't so you're going to write it from scratch:
 
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+];
+
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+    if(typeof obj === "function") {
+      return false;
+    };
+
+    if(Array.isArray(obj)) {
+      if(obj.length === 0) {
+        return "[]";
+      } else {
+      var str = "[";
+      obj.forEach( function(piece) {
+        str += stringifyJSON(piece) + ",";
+      });
+      return str.slice(0, -1) + "]" ; //slice off last comma, close array
+      }
+    };
+
+    if(obj && typeof obj === "object") {
+      if( Object.keys(obj).length === 0 ) {
+        return "{}";
+      } else {
+      var res = [];
+        Object.keys(obj).forEach( function(key) {
+          if(typeof obj[key] !== "function" && obj[key] !== undefined) {
+            var k = stringifyJSON(key);
+            var val = stringifyJSON(obj[key]);
+            res.push( "" + k + ":" + val);
+          };
+
+        });
+      return "{" + res.join() + "}";
+      }
+    };
+
+    if(typeof obj === "string") {
+      return "\"" + obj + "\""
+    };
+
+    return String(obj);
+
+  };
